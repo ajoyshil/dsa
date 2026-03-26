15bf8df09308ff8c2a965444918239b24bd95321
Ajoy <ajoy@Ajoys-MacBook-Pro.local>
2026-03-26 13:27:05 +0530

    two sum implemented

--- array/two_sum.py
+++ array/two_sum.py
@@ -8,3 +8,23 @@
 # Output: [1, 2]
 
 
+# considering array is sorted 
+
+def twosum(num, target):
+    num = sorted(num)
+    l,r = 0, len(num)-1
+    current_sum = 0
+    while l < r:
+        current_sum = num[l] + num[r]
+        if current_sum == target:
+            return (l,r)
+        if current_sum > target:
+            r -= 1
+        elif current_sum < target:
+            l += 1
+    return (-1, -1)
+
+nums = [2, 7, 11, 15]
+target = 9  
+
+print(twosum(nums, target))
